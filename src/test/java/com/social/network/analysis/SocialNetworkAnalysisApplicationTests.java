98a43d2c58b21d8016a7fa9c76f05068ba945fe3
Bayvao <vermabayvao@gmail.com>
2024-10-14 17:13:35 +0530

    first commit

---
+++ src/test/java/com/social/network/analysis/SocialNetworkAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.network.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
