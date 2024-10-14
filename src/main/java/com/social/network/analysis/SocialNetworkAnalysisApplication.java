58a01892c376f6f145a9a91fb5b18b7af95584ad
Bayvao <vermabayvao@gmail.com>
2024-10-14 17:20:29 +0530

    Configured application to use virtual threads

--- src/main/java/com/social/network/analysis/SocialNetworkAnalysisApplication.java
+++ src/main/java/com/social/network/analysis/SocialNetworkAnalysisApplication.java
@@ -1,13 +1,29 @@
 package com.social.network.analysis;
 
+import org.apache.coyote.ProtocolHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class SocialNetworkAnalysisApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SocialNetworkAnalysisApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkAnalysisApplication.class, args);
 	}
 
+	@Bean
+	public TomcatProtocolHandlerCustomizer<ProtocolHandler> protocolHandlerVirtualThreadExecutorCustomizer() {
+		return protocolHandler -> {
+			log.info("Configuring {} to use VirtualThreadPerTaskExecutor", protocolHandler);
+			protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+		};
+	}
 }
